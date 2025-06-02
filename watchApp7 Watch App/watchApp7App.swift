1a3908d4cd19ee98993910b1bb7597c01f3a474a
Tolga Bilgis <tolgabilgis@gmail.com>
2025-06-01 21:29:03 -0400

    Initial Commit

---
+++ watchApp7 Watch App/watchApp7App.swift
@@ -0,0 +1,17 @@
+//
+//  watchApp7App.swift
+//  watchApp7 Watch App
+//
+//  Created by Tolga Bilgis on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct watchApp7_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watchApp7 Watch AppTests/watchApp7_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  watchApp7_Watch_AppTests.swift
+//  watchApp7 Watch AppTests
+//
+//  Created by Tolga Bilgis on 6/1/25.
+//
+
+import Testing
+@testable import watchApp7_Watch_App
+
+struct watchApp7_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
